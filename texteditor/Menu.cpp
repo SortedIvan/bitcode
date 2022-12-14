b888f45ead1e696dbe79a5cb93c26fb9efa0bca9
Ovcharov,Ivan <450754@student.fontys.nl>
2022-12-14 16:23:42 +0100

    update: Text highlighting and scrolling through options

--- texteditor/Menu.cpp
+++ texteditor/Menu.cpp
@@ -9,7 +9,12 @@
 #include "TextHandler.h"
 #include "TextDocument.h"
 
+#include <thread>
+using namespace std::chrono_literals;
 
+
+void OnArrowsUpAndDownPress(sf::Event event, int& menu_counter, int menu_options_amount);
+void HighlightSelectedOption(std::vector<sf::Text>& text_options, sf::Color highlight_color, int selected);
 
 bool Menu::CreateNewTextDocument(std::string name, TextHandler& handler)
 {
@@ -48,11 +53,13 @@ std::vector<sf::Text> Menu::GetAllTextOptions(TextHandler& handler, sf::Font& fo
 
 // ACCESS VIOLATION ERROR
 void Menu::DrawAllOptions(sf::RenderWindow& window, std::vector<sf::Text>& menu_text_options) {
-	std::cout << menu_text_options.size() << " is the current size " << std::endl;;
 
+	//std::cout << menu_text_options.size() << " is the current size " << std::endl;
 	try {
 		for (int i = 0; i < menu_text_options.size(); i++) {
 			// EXCEPTION THROWN HERE
+			// Making sure that the text is seperated
+			menu_text_options.at(i).setPosition(sf::Vector2f(10.f, 35.f * i));
 			window.draw(menu_text_options.at(i));
 		}
 	}
@@ -64,9 +71,11 @@ void Menu::DrawAllOptions(sf::RenderWindow& window, std::vector<sf::Text>& menu_
 
 
 std::string Menu::MenuControl(TextHandler& handler) {
+	//sf::Text::findCharacterPos() IMPORTANT!
 	Utility utility;
 	sf::RenderWindow menu_window(sf::VideoMode(800, 600), "Menu");
 	sf::Color backgroundColor(0, 32, 63);
+	sf::Color highlight_text_color(173, 239, 209);
 	sf::Event event;
 	sf::Font font;
 	sf::Text menu_text;
@@ -74,6 +83,8 @@ std::string Menu::MenuControl(TextHandler& handler) {
 
 	std::vector<sf::Text> menu_text_options = GetAllTextOptions(handler, font);
 	int menu_counter = 0; // Start at the first document, count goes up and down with arrows
+	int menu_options_amount = menu_text_options.size();
+
 
 	menu_text.setOrigin(sf::Vector2f(-20.f, 0.f));
 	menu_text.setFont(font);
@@ -81,9 +92,10 @@ std::string Menu::MenuControl(TextHandler& handler) {
     while (menu_window.isOpen()) {
         // DRAWING
         menu_window.clear(backgroundColor); // Setting menu background color
-        //window.draw(background_overlay);
-		
-		DrawAllOptions(menu_window, menu_text_options); //ACCESS VIOLATION ERROR
+
+		HighlightSelectedOption(menu_text_options, sf::Color::Black, menu_counter);
+
+		DrawAllOptions(menu_window, menu_text_options);
 
         // DISPLAYING
         menu_window.display();
@@ -105,6 +117,7 @@ std::string Menu::MenuControl(TextHandler& handler) {
 					break;
 
 				case sf::Event::KeyReleased:
+					OnArrowsUpAndDownPress(event, menu_counter, menu_options_amount);
 					break;
 			}
 
@@ -115,3 +128,33 @@ std::string Menu::MenuControl(TextHandler& handler) {
 }
 
 
+void OnArrowsUpAndDownPress(sf::Event event, int& menu_counter, int menu_options_amount) 
+{
+	if (event.key.code == sf::Keyboard::Up)
+	{
+		if (menu_counter - 1 >= 0) 
+		{
+			menu_counter -= 1;
+			std::cout << menu_counter;
+		}
+	}
+	if (event.key.code == sf::Keyboard::Down)
+	{
+		if (menu_counter + 1 <= (menu_options_amount - 1)) 
+		{
+			menu_counter += 1;
+			std::cout << menu_counter;
+		}
+	}
+}
+
+void HighlightSelectedOption(std::vector<sf::Text>& text_options, sf::Color highlight_color, int selected) 
+{
+	for (int i = 0; i < text_options.size(); i++) {
+		if (i != selected) {
+			text_options.at(i).setFillColor(sf::Color::White);
+			continue;
+		}
+		text_options.at(i).setFillColor(highlight_color);
+	}
+}
